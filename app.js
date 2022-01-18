40bc0dc6ffb0546c50e339860f53927db937f6f6
AustinKammerer <austin4191@gmail.com>
2022-01-18 08:35:57 -0600

    database seeded

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
   useNewUrlParser: true,
@@ -17,6 +18,17 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('vies', path.join(__dirname, 'views'));
+
+// ************
+// MIDDLEWARE
+// ************
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+// **********
+// ROUTES
+// **********
 
 app.get('/', (req, res) => {
   res.render('home');
