973fa1d68af9273c3cbc7c0c75ac78a30b84fe64
AustinKammerer <austin4191@gmail.com>
2022-01-18 09:12:42 -0600

    form and route for adding campgrounds

--- app.js
+++ app.js
@@ -40,13 +40,19 @@ app.get('/campgrounds', async (req, res) => {
   res.render('campgrounds/index', { campgrounds });
 });
 
+app.get('/campgrounds/new', (req, res) => {
+  res.render('campgrounds/new');
+});
+
 app.get('/campgrounds/:id', async (req, res) => {
   const campground = await Campground.findById(req.params.id);
   res.render('campgrounds/show', { campground });
 });
 
-app.get('/campgrounds/new', (req, res) => {
-  res.render('campgrounds/new');
+app.post('/campgrounds', async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
 });
 
 app.listen(3000, () => {
