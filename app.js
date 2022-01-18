14ce88d1e38c63740c2b1b52a43f69b507cbede6
AustinKammerer <austin4191@gmail.com>
2022-01-18 08:18:29 -0600

    database connected

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MONGO CONNECTION ERROR:'));
+db.once('open', () => {
+  console.log('MONGO DB CONNECTED');
+});
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('vies', path.join(__dirname, 'views'));
