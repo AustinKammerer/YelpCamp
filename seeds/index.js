09413b50d5de0b5637891d37a959e4f6240332c1
AustinKammerer <austin4191@gmail.com>
2022-01-19 13:53:56 -0600

    added random images and styles

--- seeds/index.js
+++ seeds/index.js
@@ -23,9 +23,14 @@ const seedDB = async () => {
   for (let i = 0; i < 50; i++) {
     // get a random index from the cities array
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: 'https://source.unsplash.com/collection/483251',
+      description:
+        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et veritatis rem delectus earum, tempore velit, voluptatem ducimus, voluptates doloremque necessitatibus magnam odit. A, officia eius distinctio consectetur optio reprehenderit velit?',
+      price,
     });
     await camp.save();
   }
