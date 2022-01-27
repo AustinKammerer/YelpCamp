88c1eb216527f21b89e0d2356a7f1611a4c87fef
AustinKammerer <austin4191@gmail.com>
2022-01-27 17:32:08 -0600

    added error flash messages for campground GET

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -37,6 +37,10 @@ router.get(
     const campground = await Campground.findById(req.params.id).populate(
       'reviews'
     );
+    if (!campground) {
+      req.flash('error', 'Campground not found!');
+      return res.redirect('/campgrounds');
+    }
     res.render('campgrounds/show', { campground });
   })
 );
@@ -46,6 +50,10 @@ router.get(
   '/:id/edit',
   catchAsync(async (req, res, next) => {
     const campground = await Campground.findById(req.params.id);
+    if (!campground) {
+      req.flash('error', 'Campground not found!');
+      return res.redirect('/campgrounds');
+    }
     res.render('campgrounds/edit', { campground });
   })
 );
