cbfec57b4cf3090ecbf512d5c1c6d5880ed673d7
AustinKammerer <austin4191@gmail.com>
2022-01-28 13:32:44 -0600

    added currentUser and conditionally render navbar links with it

--- routes/users.js
+++ routes/users.js
@@ -35,10 +35,16 @@ router.post(
     failureFlash: true,
     failureRedirect: '/login',
   }),
-  catchAsync(async (req, res) => {
+  (req, res) => {
     req.flash('success', 'Welcome Back!');
     res.redirect('/campgrounds');
-  })
+  }
 );
+
+router.get('/logout', (req, res) => {
+  req.logout();
+  req.flash('success', 'Logged you out!');
+  res.redirect('/campgrounds');
+});
 
 module.exports = router;
