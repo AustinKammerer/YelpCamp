54b30cdce89e17744b8530602df2382eaf74a925
AustinKammerer <austin4191@gmail.com>
2022-01-29 15:29:38 -0600

    orgainized routes by endpoints

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -5,46 +5,33 @@ const catchAsync = require('../utils/catchAsync');
 const campgrounds = require('../controllers/campgrounds');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 
-// GET list of all campgrounds
-router.get('/', catchAsync(campgrounds.index));
+router
+  .route('/')
+  .get(catchAsync(campgrounds.index))
+  .post(
+    isLoggedIn,
+    validateCampground,
+    catchAsync(campgrounds.createCampground)
+  );
 
-// GET form for adding campgrounds
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
-// GET a campground by id
-router.get('/:id', catchAsync(campgrounds.showCampground));
+router
+  .route('/:id')
+  .get(catchAsync(campgrounds.showCampground))
+  .put(
+    isLoggedIn,
+    isAuthor,
+    validateCampground,
+    catchAsync(campgrounds.updateCampground)
+  )
+  .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
-// GET form for editing campground
 router.get(
   '/:id/edit',
   isLoggedIn,
   isAuthor,
   catchAsync(campgrounds.renderEditForm)
-);
-
-// POST a new campground to db
-router.post(
-  '/',
-  isLoggedIn,
-  validateCampground,
-  catchAsync(campgrounds.createCampground)
-);
-
-// PUT for updating a campground
-router.put(
-  '/:id',
-  isLoggedIn,
-  isAuthor,
-  validateCampground,
-  catchAsync(campgrounds.updateCampground)
-);
-
-// DELETE a specific campground by id
-router.delete(
-  '/:id',
-  isLoggedIn,
-  isAuthor,
-  catchAsync(campgrounds.deleteCampground)
 );
 
 module.exports = router;
