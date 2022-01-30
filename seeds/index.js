98a423fdd6bac242b908378036f1aa486780f605
AustinKammerer <austin4191@gmail.com>
2022-01-30 12:03:10 -0600

    added cluster map to index view and virtual property to Campground for custom popup

--- seeds/index.js
+++ seeds/index.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
-const cities = require('./cities');
-const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
-const mapboxToken = process.env.MAPBOX_TOKEN;
-const geocoder = mbxGeocoding({ accessToken: mapboxToken });
 
+const cities = require('./cities');
 const { places, descriptors, images } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
@@ -25,21 +21,20 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
   await Campground.deleteMany({});
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 300; i++) {
     // get a random index from the cities array
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
-    const location = `${cities[random1000].city}, ${cities[random1000].state}`;
-    const geoData = await geocoder
-      .forwardGeocode({
-        query: location,
-        limit: 1,
-      })
-      .send();
     const camp = new Campground({
       author: '61f43c736abb7a306e75bc89',
-      location,
-      geometry: geoData.body.features[0].geometry,
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      geometry: {
+        type: 'Point',
+        coordinates: [
+          cities[random1000].longitude,
+          cities[random1000].latitude,
+        ],
+      },
       title: `${sample(descriptors)} ${sample(places)}`,
       images: sample(images),
       description:
